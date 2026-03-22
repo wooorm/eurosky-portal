8abb9136bdcd59f739fc42ccfa7d65c3e7a86976
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-03-22 23:38:32 +0100

    Improve focus rings for cards

--- inertia/lib/card.tsx
+++ inertia/lib/card.tsx
@@ -25,7 +25,7 @@ export function ClickableCard({
       className={clsx(
         className,
         'overflow-hidden rounded-lg bg-white shadow-sm dark:bg-gray-800/50 dark:shadow-none dark:outline dark:-outline-offset-1 dark:outline-white/10',
-        'focus:outline-2 focus:outline-solid! focus:outline-offset-2 focus:outline-blue-600 hover:-outline-offset-1 border border-white dark:border-slate-800 hover:border-slate-300 dark:hover:border-slate-500 hover:shadow-lg'
+        'focus-visible:outline-2 focus-visible:outline-solid! focus-visible:outline-offset-2 focus-visible:outline-blue-600 hover:-outline-offset-1 border border-white dark:border-slate-800 hover:border-slate-300 dark:hover:border-slate-500 hover:shadow-lg'
       )}
     >
       {children}
