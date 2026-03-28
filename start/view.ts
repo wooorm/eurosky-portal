79915263b2833c8d3ab0888f73760668298d8f35
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-03-28 18:22:39 +0100

    Improve opengraph support

--- start/view.ts
+++ start/view.ts
@@ -1,4 +1,10 @@
 import edge from 'edge.js'
 import { edgeMarkdown } from 'edge-markdown'
+import env from '#start/env'
+
+const APP_URL = new URL('/', env.get('APP_URL')).toString()
 
 edge.use(edgeMarkdown, {})
+
+edge.global('opengraph_url', APP_URL)
+edge.global('service_domain', new URL(APP_URL).host)
