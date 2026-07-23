d4b1df169aeb5430636bf357b8731fbd9483bc48
Titus Wormer <tituswormer@gmail.com>
2026-07-23 16:20:36 +0200

    reword

--- config/queue.ts
+++ config/queue.ts
@@ -6,11 +6,13 @@ export default defineConfig({
   locations: ['./app/jobs/**/*.{js,ts}'],
   worker: {
     /**
-     * Max backfills allowed at once; prevents problems on bursts at launch /
+     * Max jobs allowed at once; prevents problems on bursts at launch /
      * wild pathological cases.
      *
+     * For now, there’s only the backfill job.
      * The current choice `200` is a plausible value to start with.
      * Should be revisited later.
+     * At that point moving it to another process may be a good idea.
      */
     concurrency: 200,
   },
